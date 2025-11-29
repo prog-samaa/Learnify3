f3b99795b347361b3a69f692c06dcc7a3ca29f6f
Samaa Mohamed <samaamohamed182006@gmail.com>
2025-11-29 12:57:31 +0200

    Update CourseRepository.kt

--- app/src/main/java/com/example/learnify/data/repository/CourseRepository.kt
+++ app/src/main/java/com/example/learnify/data/repository/CourseRepository.kt
@@ -12,7 +12,7 @@ import kotlin.math.log10
 
 class CourseRepository(private val dao: CourseDao) {
 
-    private val apiKey = "AIzaSyD_Nntb2yllML53TAqll67lTzZaJNKFe3w"
+    private val apiKey = "Yor API key"
 
     // Returns LiveData of trending courses for a specific category
     fun getTrendingLive(category: String): LiveData<List<CourseEntity>> =
